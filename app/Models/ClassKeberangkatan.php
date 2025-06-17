8888dfba0624cae5b6cfd61306fc1eede1a89f70
Muzakky Abdullah <muzakkyabdullah93@gmail.com>
2025-06-18 05:24:38 +0700

    Push project skripsi pertama

---
+++ app/Models/ClassKeberangkatan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassKeberangkatan extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/KursiKeberangkatan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KursiKeberangkatan extends Model
+{
+    use HasFactory;
+}
