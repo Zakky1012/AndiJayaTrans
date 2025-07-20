7b0d11136317e99832eabef5baa68fbebcfb7d50
Muzakky Abdullah <muzakkyabdullah93@gmail.com>
2025-07-20 11:28:07 +0700

    ada kesalahan di overview yang ditampilkan stats nya adalah amount paid

--- app/Filament/Widgets/TransaksiOverview.php
+++ app/Filament/Widgets/TransaksiOverview.php
@@ -13,7 +13,7 @@ class TransaksiOverview extends BaseWidget
         return [
             Stat::make('Total Transaksi', Transaksi::query()->count()),
             Stat::make('Total Amount (Pending)', 'Rp. ' . number_format(Transaksi::query()->where('status_payment', 'pending')->sum('grand_total'),'0',',','.')),
-            Stat::make('Total Amount (Pending)', 'Rp. ' . number_format(Transaksi::query()->where('status_payment', 'dibayar')->sum('grand_total'),'0',',','.')),
+            Stat::make('Total Amount (Paid)', 'Rp. ' . number_format(Transaksi::query()->where('status_payment', 'dibayar')->sum('grand_total'),'0',',','.')),
         ];
     }
 }
